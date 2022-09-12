0ed1df9e21a203bc5213c752571f64d553f281c3
pcorwin <phoebe.corwin23@gmail.com>
2022-09-12 13:06:24 -0400

    Added floor, gravity to simulation

--- simulate.py
+++ simulate.py
@@ -1,8 +1,14 @@
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)    #connect
+p.setAdditionalSearchPath(
+    pybullet_data.getDataPath())
 
+p.setGravity(0,0,-9.8)              #creates gravity in simulated environment
+
+planeID = p.loadURDF("plane.urdf")  #imports 'floor' for simulated environment
 p.loadSDF("box.sdf")                #includes box created in generate.py for simulation
 
 for i in range(0, 1000):            #simulation loop
