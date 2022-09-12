53d8876c62f7b93160b559a8641930db0ea89320
pcorwin <phoebe.corwin23@gmail.com>
2022-09-12 18:33:47 -0400

    Motor work and analysis of motor function added to simulate.py and analysis.py

--- analyze.py
+++ analyze.py
@@ -3,8 +3,16 @@ import matplotlib.pyplot as mp
 
 bLegSensorValues = numpy.load("data/bLeg.npy")
 fLegSensorValues = numpy.load("data/fLeg.npy")
+targetAnglesF = numpy.load("data/frontMotorAngles.npy")
+targetAnglesB = numpy.load("data/backMotorAngles.npy")
 
 mp.plot(bLegSensorValues,label= "BackLeg")
 mp.plot(fLegSensorValues,label= "FrontLeg")
 mp.legend()
 mp.show()
+
+mp.plot(targetAnglesF,label= "Front")
+mp.plot(targetAnglesB,label= "Back")
+mp.legend()
+mp.show()
+
--- simulate.py
+++ simulate.py
@@ -1,6 +1,9 @@
+import math
 import time
 import numpy
+import random
 
+import numpy as np
 import pybullet as p
 import pybullet_data
 
@@ -23,16 +26,45 @@ pyrosim.Prepare_To_Simulate(robotID)
 bLegSensorValues = numpy.zeros(STEPS)
 fLegSensorValues = numpy.zeros(STEPS)
 
+targetAnglesFront = (numpy.linspace(-numpy.pi/4,numpy.pi/4,STEPS))
+amplitudeF = numpy.pi/4
+frequencyF = 10
+phaseOffsetF = 0
+
+targetAnglesBack = (numpy.linspace(-numpy.pi/4,numpy.pi/4,STEPS))
+amplitudeB = numpy.pi/4
+frequencyB = 10
+phaseOffsetB = 5
+
+
 for i in range(0, STEPS):            #simulation loop
+    targetAnglesFront[i] = amplitudeF * np.sin(frequencyF * targetAnglesFront[i] + phaseOffsetF)
+    targetAnglesBack[i] = amplitudeB * np.sin(frequencyB * targetAnglesBack[i] + phaseOffsetB)
     p.stepSimulation()
     bLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BLeg")
     fLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FLeg")
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex=1,
+        jointName="Torso_BLeg",
+        controlMode=2,
+        targetPosition=targetAnglesBack[i],
+        maxForce=500
+    )
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex=1,
+        jointName="Torso_FLeg",
+        controlMode=2,
+        targetPosition=targetAnglesFront[i],
+        maxForce=500
+    )
     time.sleep(1/60)                #time between each step
-    #if i % 100 == 0:                 #output timestamp for ease of use
-    #    print(int(i/100))
+    if i % 100 == 0:                 #output timestamp for ease of use
+        print(int(i/100))
 
 numpy.save("data/bLeg",bLegSensorValues)
 numpy.save("data/fLeg",fLegSensorValues)
+numpy.save("data/frontMotorAngles",targetAnglesFront)
+numpy.save("data/backMotorAngles",targetAnglesBack)
 
 
 p.disconnect()                      #disconnect
