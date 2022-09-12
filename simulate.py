3a4c41fa41167078f75f0dd25a9ede5c1958b350
pcorwin <phoebe.corwin23@gmail.com>
2022-09-12 12:59:21 -0400

    Added box.sdf from generate.py to simulation.py

--- simulate.py
+++ simulate.py
@@ -3,6 +3,8 @@ import time
 
 physicsClient = p.connect(p.GUI)    #connect
 
+p.loadSDF("box.sdf")                #includes box created in generate.py for simulation
+
 for i in range(0, 1000):            #simulation loop
     p.stepSimulation()
     time.sleep(1/60)                #time between each step
