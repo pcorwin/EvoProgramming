fdab4bc1628347df794bfeaecab594d80ade1a88
pcorwin <phoebe.corwin23@gmail.com>
2022-09-12 13:24:29 -0400

    Added 10x10x2 'pallet' of boxes via generate.py modification

--- generate.py
+++ generate.py
@@ -10,7 +10,12 @@ z = 0.5     #initial z coord
 
 pyrosim.Start_SDF("boxes.sdf")
 
-pyrosim.Send_Cube(name="Box1", pos=[x,y,z], size=[l,w,h])
-pyrosim.Send_Cube(name="Box2", pos=[x,y,z+1], size=[l,w,h])
+for i in range(0,10):
+    for j in range(0,10):
+        for k in range(0,2):
+            pyrosim.Send_Cube(name=f"Box[{i+1},{j+1}, {z+1}]", pos=[x + i, y + j, z + k], size=[l, w, h])
+
+#pyrosim.Send_Cube(name="Box1", pos=[x,y,z], size=[l,w,h])
+#pyrosim.Send_Cube(name="Box2", pos=[x,y,z+1], size=[l,w,h])
 
 pyrosim.End()
