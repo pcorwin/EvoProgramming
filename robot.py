107a633fe40c380e38e08f64f9c301c9a4ae5cf0
pcorwin <phoebe.corwin23@gmail.com>
2022-10-04 13:02:08 -0400

    Fixed bug; review dict operators. pushing corrections and moving on (@ step 71)

--- robot.py
+++ robot.py
@@ -41,6 +41,8 @@ class ROBOT:
     def Act(self, t):
         for neuronName in self.nn.Get_Neuron_Names():
             if self.nn.Is_Motor_Neuron(neuronName):
+                jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
+                desiredAngle = self.nn.Get_Value_Of(neuronName)
                 print(neuronName)
         #for m in self.motors.values():
         #    m.Set_Value(t)
